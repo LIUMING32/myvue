241c3e21abf8e845ea51b43c219b8039c82f360d
liuming <asxw00@126.com>
2017-11-02 18:11:34 +0800

    嵌套路由

---
+++ src/components/children/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+    <div style="margin-top: 40px;">
+    	<el-input v-model="input" placeholder="请输入验证码"></el-input>
+    	<el-button type="warning"  >获取验证码</el-button>     	
+    </div>
+    <div style="margin-top: 20px;">
+    	<a href="https://www.baidu.com"><el-button type="warning"   style="padding:10px 127px;" >登录\注册</el-button></a>    	 
+    </div>  
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'LMBL',
+      input: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.el-input{padding: 0px;margin: 0px;width: 200px;}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  	 <h1>回家</h1>
+    <headerdiv logo="Wise"></headerdiv>
+    <router-view></router-view>
+    <footerdiv></footerdiv>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'LMBL',
+      input: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
